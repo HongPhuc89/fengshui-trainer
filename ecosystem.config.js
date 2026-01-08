949f8f65d4cb66017c0cee2115501521e7fe4f92
Nguyen Hong Phuc <scottfu89@gmail.com>
2026-01-08 13:34:06 +0700

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 module.exports = {
     apps: [
         {
             name: 'backend',
-            cwd: './apps/backend',
-            script: 'npm',
-            args: 'run start:prod',
+            cwd: path.join(__dirname, 'apps/backend'),
+            script: 'dist/src/main.js',
             instances: 1,
             autorestart: true,
             watch: false,
@@ -12,15 +13,13 @@ module.exports = {
             env: {
                 NODE_ENV: 'production',
                 PORT: 3000,
-                NODE_CONFIG_DIR: '../../config',
+                NODE_CONFIG_DIR: path.join(__dirname, 'config'),
             },
-            error_file: '~/.pm2/logs/backend-error.log',
-            out_file: '~/.pm2/logs/backend-out.log',
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
         },
         {
             name: 'admin',
-            cwd: './apps/admin',
+            cwd: path.join(__dirname, 'apps/admin'),
             script: 'npx',
             args: 'serve -s dist -l 3001',
             instances: 1,
@@ -30,8 +29,6 @@ module.exports = {
             env: {
                 NODE_ENV: 'production',
             },
-            error_file: '~/.pm2/logs/admin-error.log',
-            out_file: '~/.pm2/logs/admin-out.log',
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
         },
     ],
