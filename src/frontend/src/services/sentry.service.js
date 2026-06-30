0323691a44a233e10d942c2f38899b4b5b6072e4
Nguyen Hong Phuc <scottfu89@gmail.com>
2026-06-30 11:26:43 +0700

    feat: enhance Sentry tracking by adding metrics for login and content load events

--- src/frontend/src/services/sentry.service.js
+++ src/frontend/src/services/sentry.service.js
@@ -22,14 +22,23 @@ const sentryService = {
 
   trackLogin(user) {
     Sentry.logger.info('User logged in', { email: user.email })
+    Sentry.metrics.count('auth.login.success', 1, {
+      attributes: { email: user.email },
+    })
   },
 
   trackVideoLoad(courseSlug, lessonSlug) {
     Sentry.logger.info('Video load success', { course: courseSlug, lesson: lessonSlug })
+    Sentry.metrics.count('video.load.success', 1, {
+      attributes: { course_slug: courseSlug, lesson_slug: lessonSlug },
+    })
   },
 
   trackPdfLoad(bookSlug, chapterOrder) {
     Sentry.logger.info('PDF load success', { book: bookSlug, chapter: chapterOrder })
+    Sentry.metrics.count('pdf.load.success', 1, {
+      attributes: { book_slug: bookSlug, chapter_order: chapterOrder },
+    })
   },
 }
 
